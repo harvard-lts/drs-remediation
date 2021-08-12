c15416d4d3e1747b81435d9d5ce7ee5b63ec80c1
William Welling <wwelling@library.tamu.edu>
2021-08-12 15:51:45 -0500

    remove unused import

--- src/main/java/edu/harvard/s3/store/AmazonS3Bucket.java
+++ src/main/java/edu/harvard/s3/store/AmazonS3Bucket.java
@@ -27,7 +27,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
